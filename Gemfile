3e665b1becaa00744942f12fb1560574bbed2017
Ubuntu <ubuntu@ip-172-31-87-29.ec2.internal>
2019-11-15 15:24:49 +0000

    add devise user

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'coffee-rails', '~> 4.2'
 # gem 'therubyracer', platforms: :ruby
 
 
-gem 'devise', '4.3.0'
+gem 'devise', '4.6.0'
 
 
 # Use jquery as the JavaScript library
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  
   root 'static_pages#home'
   
   get '/booking' => 'static_pages#booking'
   
   get '/about' => 'static_pages#about'
   
+  root :to => 'static_pages#home'
   
   # get 'static_pages/home'
   # get 'static_pages/booking'
